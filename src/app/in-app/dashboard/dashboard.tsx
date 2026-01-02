584e96cd369f2b68a89b5cbf92ebff0a9245850f
octaviomds <151770584+octaviomds@users.noreply.github.com>
2026-01-02 20:43:56 +0100

    dashboard.tsx

--- src/app/in-app/dashboard/dashboard.tsx
+++ src/app/in-app/dashboard/dashboard.tsx
@@ -13,45 +13,6 @@ export default function Dashboard() {
   return (
     <>
       <div className={classes("row-layout dashboard-container")}>
-        <IgrNavbar className={classes("navbar")}>
-          <div style={{display: 'contents'}} slot="start" key={uuid()}>
-            <IgrIconButton variant="flat" className={classes("icon-button")}>
-              <span className={classes("material-icons")} key={uuid()}>
-                <span key={uuid()}>menu</span>
-              </span>
-              <IgrRipple key={uuid()}></IgrRipple>
-            </IgrIconButton>
-          </div>
-          <div className={classes("row-layout group")} key={uuid()}>
-            <h6 className={classes("h6")}>
-              <span>ECORIDE</span>
-            </h6>
-          </div>
-          <div style={{display: 'contents'}} slot="end" key={uuid()}>
-            <IgrIconButton variant="flat" className={classes("icon-button")}>
-              <span className={classes("material-icons")} key={uuid()}>
-                <span key={uuid()}>search</span>
-              </span>
-              <IgrRipple key={uuid()}></IgrRipple>
-            </IgrIconButton>
-          </div>
-          <div style={{display: 'contents'}} slot="end" key={uuid()}>
-            <IgrIconButton variant="flat" className={classes("icon-button")}>
-              <span className={classes("material-icons")} key={uuid()}>
-                <span key={uuid()}>favorite</span>
-              </span>
-              <IgrRipple key={uuid()}></IgrRipple>
-            </IgrIconButton>
-          </div>
-          <div style={{display: 'contents'}} slot="end" key={uuid()}>
-            <IgrIconButton variant="flat" className={classes("icon-button")}>
-              <span className={classes("material-icons")} key={uuid()}>
-                <span key={uuid()}>more_vert</span>
-              </span>
-              <IgrRipple key={uuid()}></IgrRipple>
-            </IgrIconButton>
-          </div>
-        </IgrNavbar>
         <p className={classes("typography__caption text")}>
           <span>EcoRide est la principale plateforme de covoiturage pour les voyageurs de l' environnement et ceux qui recherchent une solution économique pour leur déplacements</span>
         </p>
